db736b1f7256e5572f7da5ee360c536c33d7b76d
Zayd Sarraj <zayd.sarraj@gmail.com>
2019-11-23 18:29:35 -0500

    listed guesses, prevented duplicate guesses, final submission

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,9 +6,10 @@ var guessesLeftText = document.getElementById("guessesLeftdiv");
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
+var theGuesses = [];
 // array for letters 
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// logs the computer guess as a function then uses the function
+// logs the computer guess as a function, then uses the function to choose a letter
 function guessGame() { computerGuess = letters[Math.floor(Math.random() * letters.length)] }
 guessGame()
 console.log(computerGuess)
@@ -16,12 +17,18 @@ console.log(computerGuess)
 document.onkeyup = function (event) {
         // logs your guess
         var yourGuess = event.key;
-        var theGuesses = [];
         console.log(yourGuess)
-
-        // compares the guesses, if not equal you lose a guess and it lists your guess
+        // function to check if you used a letter already, then lists it if it's a new guess
+        function checkGuess() {
+                if (theGuesses.includes(yourGuess) === false) {
+                        theGuesses.push(yourGuess);
+                        guessesLeft--;
+                        console.log(theGuesses)
+                }
+        }
+        // compares the guesses, if not equal you lose a guess and it lists your guess, only if it's a new guess
         if (yourGuess !== computerGuess) {
-                guessesLeft--;
+                checkGuess();
         }
         //  if they're equal you win and start over
         if (yourGuess === computerGuess) {
@@ -44,4 +51,3 @@ document.onkeyup = function (event) {
         yourGuessesText.textContent = "Your guesses so far: " + theGuesses;
         guessesLeftText.textContent = "Guesses left: " + guessesLeft;
 }
-console.log(guessesLeft)
